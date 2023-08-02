017a9719814ac15d21b13bb749e98bbae4594c77
Marcos-Echeverria <marcosalbertoecheverria@gmail.com>
2023-08-02 18:47:45 -0300

    conexion mongo

--- taskapi/src/tasks/tasks.service.ts
+++ taskapi/src/tasks/tasks.service.ts
@@ -2,16 +2,18 @@ import { Injectable } from '@nestjs/common';
 import {InjectModel} from '@nestjs/mongoose'
 import {Task} from '../schemas/task.schema'
 import { Model } from 'mongoose';
+import { CreateTaskDto } from 'src/dto/create-task.dto';
+import { UpdateTaskDto } from 'src/dto/update-task.dto';
 
 @Injectable()
 export class TasksService {
-    constructor(@InjectModel(Task.name)private taskModel: Model<Task>){}
+    constructor(@InjectModel(Task.name) private taskModel: Model<Task>){}
 
     findAll(){
-        this.taskModel.find();
+        return this.taskModel.find();
     }
 
-    async create(createTask:any){
+    async create(createTask:CreateTaskDto){
         const newTask = this.taskModel.create(createTask);
         return (await newTask).save();
     }
@@ -24,7 +26,7 @@ export class TasksService {
         return this.taskModel.findByIdAndDelete(id);
     }
 
-    async update(id:string, task:any){
-        return this.taskModel.findByIdAndUpdate(id, task);
+    async update(id:string, task:UpdateTaskDto){
+        return this.taskModel.findByIdAndUpdate(id, task, {new: true});
     }
 }
