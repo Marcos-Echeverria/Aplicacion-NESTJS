00a07694eeb09fb8670714156b90952e1fe817fd
Marcos-Echeverria <marcosalbertoecheverria@gmail.com>
2023-08-02 19:12:06 -0300

    Agregando React al proyecto

---
+++ tasksfront/src/App.tsx
@@ -0,0 +1,19 @@
+import TaskList from "./components/TaskList";
+import TaskForm from "./components/taskForm"; //Aca puede haber un conflicto a futuro porque tiene una miniscula donde deberia haber mayuscula pero nose porque no me deja ponerlo asi
+
+export const App = () => {
+  return (
+    
+    <div className="bg-zinc-900 h-screen text-white flex items-center justify-center">
+    <div className="bg-gray-950 p-4 w-2/5">
+      <h1 className="text-3xl font-bold text-center block my-2">Tasks App</h1>
+
+      <TaskForm/>
+      <TaskList/>
+    </div>
+    </div>
+    
+  );
+}
+
+export default App
